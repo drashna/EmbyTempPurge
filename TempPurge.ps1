824245e74a08e8dbd272e6671164a77f58e3cfaf
drashna <drashna@live.com>
2017-09-12 09:32:42 -0700

    Upload script

---
+++ TempPurge.ps1
@@ -0,0 +1,16 @@
+﻿
+$TempFolder = ,@{Path = "E:\Temp\Emby"; limit = (Get-Date).AddHours(-5)}
+$TempFolder += ,@{Path = "E:\Temp\Plex"; limit = (Get-Date).AddHours(-5)}
+$TempFolder += ,@{Path = "E:\Emby\transcoding-temp"; limit = (Get-Date).AddHours(-5)}
+$TempFolder += ,@{Path = "E:\Emby\Backup\db"; limit = (Get-Date).AddMonths(-6)}
+
+
+foreach ($folder in $TempFolder)
+{
+    # Delete files older than the $limit.
+    Get-ChildItem -Path $folder.Path -Recurse -Force | Where-Object { !$_.PSIsContainer -and ($_.CreationTime -lt $folder.limit) } | Remove-Item -Force
+    If ($test.CreationTime -lt $folder.limit)
+    { Write-Host ( "Deleting files older than " + $folder.limit + " from " + $folder.Path) }
+    # Delete any empty directories left behind after deleting the old files.
+    Get-ChildItem -Path $folder.Path -Recurse | Where-Object { $_.PSIsContainer -and (Get-ChildItem -Path $_.FullName -Recurse -Force | Where-Object { !$_.PSIsContainer }) -eq $null } | Remove-Item  -Recurse
+}
